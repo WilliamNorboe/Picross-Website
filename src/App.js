88031e21bdcedf9cd3981016fb43454a939a4f85
William Norboe <william.norboe@gmail.com>
2023-12-17 11:29:38 -0800

    set up board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+let createBoard = (size)=>{
+  let picrossBoard = [];
+  for(let i = 0; i < size; ++i){
+    let row = []
+    for(let j = 0; j < size; ++j){
+      row.push(Math.floor(Math.random() * 2));
+    }
+    picrossBoard.push(row);
+  }
+  return picrossBoard
+}
+
+
+let changeSize = (setSize) =>{
+  console.log("we");
+  let newSize = prompt("Enter a new Size for the board: ");
+  setSize(newSize);
+}
 
 function App() {
+  const [size, setSize] = useState(5);
+  let picrossAnswer = createBoard(size);
+  let board = [];
+  let row = [];
+
+  for (let i = 0; i < picrossAnswer.length; i++) {
+    row = []
+    for (let j = 0; j < picrossAnswer[i].length; j++) {
+      row.push(<div className='box'>{picrossAnswer[i][j]}</div>);
+    }
+    board.push(<div className='row'>{row}</div>);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='page'>
+        <h1>Picross</h1>
+        <button onClick = {()=>{changeSize(setSize)}} >Change Size</button>
+        {board}
+      </div>
+      
     </div>
   );
 }
