c11736366e5bf638a128da0d26769fbde0148af6
William Norboe <william.norboe@gmail.com>
2023-12-18 07:59:03 -0800

    colored boxes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ let createBoard = (size)=>{
   for(let i = 0; i < size; ++i){
     let row = []
     for(let j = 0; j < size; ++j){
-      row.push(Math.floor(Math.random() * 2));
+      row.push(Math.floor(Math.random()*2));
     }
     picrossBoard.push(row);
   }
@@ -15,7 +15,6 @@ let createBoard = (size)=>{
 
 
 let changeSize = (setSize) =>{
-  console.log("we");
   let newSize = prompt("Enter a new Size for the board: ");
   setSize(newSize);
 }
@@ -23,15 +22,22 @@ let changeSize = (setSize) =>{
 function App() {
   const [size, setSize] = useState(5);
   let picrossAnswer = createBoard(size);
-  let board = [];
+  // let board = [];
   let row = [];
-
+  const [board, setBoard] = useState([]);
+  // const [row, setRow] = useState([]);
   for (let i = 0; i < picrossAnswer.length; i++) {
     row = []
     for (let j = 0; j < picrossAnswer[i].length; j++) {
-      row.push(<div className='box'>{picrossAnswer[i][j]}</div>);
+      if(picrossAnswer[i][j] == 1){
+        row.push(<div className= 'box marked' key = {j}>{picrossAnswer[i][j]}</div>);
+      }
+      else{
+        row.push(<div className= 'box' key = {j}>{picrossAnswer[i][j]}</div>);
+      }
+      console.log(row[j]);
     }
-    board.push(<div className='row'>{row}</div>);
+    board.push(<div className='row' key = {i}>{row}</div>);
   }
 
   return (
