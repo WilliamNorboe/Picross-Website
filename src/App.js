cfc013475080482a3361725dece8c2444d42ca86
William Norboe <william.norboe@gmail.com>
2023-12-18 08:56:59 -0800

    allows user to change box colors

--- src/App.js
+++ src/App.js
@@ -3,49 +3,82 @@ import { useState } from 'react';
 
 let createBoard = (size)=>{
   let picrossBoard = [];
+  let playerBoard = [];
   for(let i = 0; i < size; ++i){
     let row = []
+    let playerRow = [];
     for(let j = 0; j < size; ++j){
       row.push(Math.floor(Math.random()*2));
+      playerRow.push(0);
     }
     picrossBoard.push(row);
+    playerBoard.push(playerRow)
   }
-  return picrossBoard
+  return [picrossBoard, playerBoard]
 }
 
 
-let changeSize = (setSize) =>{
+let changeSize = (setSize, setPlayerBoard) =>{
   let newSize = prompt("Enter a new Size for the board: ");
+  let playerBoard = [];
+  for(let i = 0; i < newSize; ++i){
+    let playerRow = [];
+    for(let j = 0; j < newSize; ++j){
+      playerRow.push(0);
+    }
+    playerBoard.push(playerRow)
+  }
+  setPlayerBoard(playerBoard)
   setSize(newSize);
 }
 
+let boxClicked = (i, j, board, setBoard) =>{
+  console.log(i + " " + j);
+  let t = structuredClone(board);
+  if(t[i][j] == 0){
+    t[i][j] = 1;
+  }
+  else{
+    t[i][j] = 0;
+  }
+  
+  setBoard(t);
+}
+
+let printb = (b1, b2) =>{
+  console.log(b1);
+  console.log(b2);
+}
 function App() {
   const [size, setSize] = useState(5);
-  let picrossAnswer = createBoard(size);
-  // let board = [];
+  let result = createBoard(size);
+  let picrossAnswer = result[0];
+  // let playerBoard = result[1];
+  const [playerBoard, setPlayerBoard] = useState(result[1]);
+  let board = [];
   let row = [];
-  const [board, setBoard] = useState([]);
+  // const [board, setBoard] = useState([]);
   // const [row, setRow] = useState([]);
-  for (let i = 0; i < picrossAnswer.length; i++) {
+  for (let i = 0; i < playerBoard.length; i++) {
     row = []
-    for (let j = 0; j < picrossAnswer[i].length; j++) {
-      if(picrossAnswer[i][j] == 1){
-        row.push(<div className= 'box marked' key = {j}>{picrossAnswer[i][j]}</div>);
+    for (let j = 0; j < playerBoard[i].length; j++) {
+      if(playerBoard[i][j] == 1){
+        row.push(<div className= 'box marked' key = {j} onClick = {()=>{boxClicked(i, j, playerBoard , setPlayerBoard)}} >{playerBoard[i][j]}</div>);
       }
       else{
-        row.push(<div className= 'box' key = {j}>{picrossAnswer[i][j]}</div>);
+        row.push(<div className= 'box' key = {j} onClick = {()=>{boxClicked(i, j, playerBoard , setPlayerBoard)}}>{playerBoard[i][j]}</div>);
       }
-      console.log(row[j]);
     }
     board.push(<div className='row' key = {i}>{row}</div>);
   }
-
+  
   return (
     <div className="App">
       <div className='page'>
         <h1>Picross</h1>
-        <button onClick = {()=>{changeSize(setSize)}} >Change Size</button>
+        <button onClick = {()=>{changeSize(setSize, setPlayerBoard)}} >Change Size</button>
         {board}
+        <button onClick = {()=>{printb(playerBoard, picrossAnswer)}} >Check</button>
       </div>
       
     </div>
